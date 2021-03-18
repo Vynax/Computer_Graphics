c1b962d356d9b0f1217faa9157c4f015b70b01d7
css-wu_win10_github_desktop <t109598085@ntut.edu.tw>
2021-03-19 02:04:11 +0800

    完成Lab3
    大概完成了 應該

--- Lab1222/Lab1222/Lab03_Transformation.cpp
+++ Lab1222/Lab1222/Lab03_Transformation.cpp
@@ -1,11 +1,20 @@
 /*
  * OGL01Shape3D.cpp: 3D Shapes
  */
+
+#define angleX 0
+#define angleY -15
+#define angleZ 0
+
 #include <windows.h>  // for MS Windows
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include <iostream>
 
  /* Global variables */
 char title[] = "3D Shapes";
+float tx = 0, ty = 0, tz = 0;
+float thetaX = 0, thetaY = angleY, thetaZ = 0;
+
 
 /* Initialize OpenGL Graphics */
 void initGL() {
@@ -22,45 +31,81 @@ void initGL() {
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
 	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
-	
 
 	// Render a pyramid consists of 4 triangles
 	glLoadIdentity();                  // Reset the model-view matrix
-	glTranslatef(-1.5f, 0.0f, -6.0f);  // Move left and into the screen
+	glTranslatef(tx, ty, tz);  // Move left and into the screen
+	gluLookAt(0, 0, 10.0f, 0, 0, 0, 0, 1, 0);
+	glRotatef(thetaX, 1, 0, 0);
+	glRotatef(thetaY, 0, 1, 0);
+	glRotatef(thetaZ, 0, 0, 1);
+
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red X axis
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(2.0f, 0.0f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green Y axis
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 2.0f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue Z axis
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 2.0f);
+
+	glEnd();
+
 
 	glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
 	   // Front
 	glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, -2.0f, 1.0f);
+	glVertex3f(1.0f, -2.0f, 1.0f);
 
 	// Right
-	glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	glVertex3f(1.0f, -1.0f, 1.0f);
 	glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, -2.0f, 1.0f);
+	glVertex3f(1.0f, -2.0f, -1.0f);
 
 	// Back
-	glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	glVertex3f(1.0f, -1.0f, -1.0f);
 	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, -2.0f, -1.0f);
+	glVertex3f(-1.0f, -2.0f, -1.0f);
 
 	// Left
-	glColor3f(1.0f, 0.0f, 0.0f);       // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);       // Blue
-	glVertex3f(-1.0f, -1.0f, -1.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);       // Green
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-	glEnd();   // Done drawing the pyramid
+	glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(-1.0f, -2.0f, -1.0f);
+	glVertex3f(-1.0f, -2.0f, 1.0f);
+
+	glColor3f(0, 1, 0); glVertex3f(-1.0f, -2.0f, 1.0f);
+	glColor3f(0, 0, 1); glVertex3f(1.0f, -2.0f, 1.0f);
+	glColor3f(1, 0, 0); glVertex3f(1.0f, -2.0f, -1.0f);
+
+
+	glColor3f(0, 0, 1); glVertex3f(-1.0f, -2.0f, 1.0f);
+	glColor3f(0, 1, 0); glVertex3f(1.0f, -2.0f, -1.0f);
+	glColor3f(1, 0, 0); glVertex3f(-1.0f, -2.0f, -1.0f);
+
+	glEnd();   // Done drawing the pyramid (triangle part)
+
+
+
+
+
+	//glBegin(GL_QUADS);
+	//glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	//glVertex3f(-1.0f, -2.0f, 1.0f);
+	//glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	//glVertex3f(1.0f, -2.0f, 1.0f);
+	//glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	//glVertex3f(1.0f, -2.0f, -1.0f);
+	//glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+	//glVertex3f(-1.0f, -2.0f, -1.0f);
+	//glEnd();
 
 	glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
 }
@@ -83,11 +128,60 @@ void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integ
 }
 
 void myKeyboard(unsigned char key, int x, int y) {
-	switch (key)
-	{
+	switch (key) {
 	case 'r':
-		//reset translation & rotation
+		thetaX = 0;
+		thetaY = angleY;
+		thetaZ = 0;
+		tx = 0;
+		ty = 0;
+		tz = 0;
+		break;
+	case 'a':
+		thetaY -= 3;
+		break;
+	case 'd':
+		thetaY += 3;
+		break;
+	case 'w':
+		thetaX -= 3;
+		break;
+	case 's':
+		thetaX += 3;
+		break;
+	case 'z':
+		thetaZ += 3;
+		break;
+	case 'x':
+		thetaZ -= 3;
+		break;
+	default:
+		break;
 	}
+
+	// std::cout << "thetaX:" << thetaX << std::endl;
+	glutPostRedisplay();
+}
+
+void mySpecialKey(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		tx -= 0.2;
+		break;
+	case GLUT_KEY_RIGHT:
+		tx += 0.2;
+		break;
+	case GLUT_KEY_UP:
+		ty += 0.2;
+		break;
+	case GLUT_KEY_DOWN:
+		ty -= 0.2;
+		break;
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
 }
 
 
@@ -101,6 +195,8 @@ int main(int argc, char** argv) {
 	glutCreateWindow(title);          // Create window with the given title
 	glutDisplayFunc(display);       // Register callback handler for window re-paint event
 	glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+	glutKeyboardFunc(myKeyboard);
+	glutSpecialFunc(mySpecialKey);
 	initGL();                       // Our own OpenGL initialization
 	glutMainLoop();                 // Enter the infinite event-processing loop
 	return 0;
