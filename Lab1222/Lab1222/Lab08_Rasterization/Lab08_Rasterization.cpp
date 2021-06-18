bd7a76108234b1f6a85bae1e3c8bc407d350ad54
Vynax_家裡桌電_bash <t109598085@ntut.edu.tw>
2021-06-18 23:39:54 +0800

    完成20210504_Lab08_Rasterization

--- Lab1222/Lab1222/Lab08_Rasterization/Lab08_Rasterization.cpp
+++ Lab1222/Lab1222/Lab08_Rasterization/Lab08_Rasterization.cpp
@@ -20,104 +20,104 @@ void arbitrary_Rotate(float a, float b, float c);
 /* Initialize OpenGL Graphics */
 
 void initGL() {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-	glClearDepth(1.0f);                   // Set background depth to farthest
-	glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
-	glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
-	glShadeModel(GL_SMOOTH);   // Enable smooth shading
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+    glClearDepth(1.0f);                   // Set background depth to farthest
+    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+    glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
+    glShadeModel(GL_SMOOTH);   // Enable smooth shading
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
 
 /* Handler for window-repaint event. Called back when the window first appears and
    whenever the window needs to be re-painted. */
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
 
-	// Render a pyramid consists of 4 triangles
-	glLoadIdentity();                  // Reset the model-view matrix
-	gluLookAt(0, 0, 15.0f, 0, 0, 0, 0, 1, 0);
-	// glRotatef(-30, 0, 1, 0);
-	//glRotatef(15, 1, 0, 0);
+    // Render a pyramid consists of 4 triangles
+    glLoadIdentity();                  // Reset the model-view matrix
+    gluLookAt(0, 0, 15.0f, 0, 0, 0, 0, 1, 0);
+    // glRotatef(-30, 0, 1, 0);
+    //glRotatef(15, 1, 0, 0);
 
-	draw_Axis();
-	draw_Click(config.clickX, config.clickY, config.clickZ, radius);
+    //draw_Axis();
+    draw_Click(config.clickX, config.clickY, config.clickZ, radius);
 
-	glLoadIdentity();                  // Reset the model-view matrix
-	gluLookAt(0, 0, 15.0f, 0, 0, 0, 0, 1, 0);
+    glLoadIdentity();                  // Reset the model-view matrix
+    gluLookAt(0, 0, 15.0f, 0, 0, 0, 0, 1, 0);
 
-	//draw_Pyramid();
+    //draw_Pyramid();
 
-	//glLoadIdentity();
+    //glLoadIdentity();
 
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	config.Draw();
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    config.Draw();
 
-	//lamp.Draw();
-	//config.Draw();
+    //lamp.Draw();
+    //config.Draw();
 
-	glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
 }
 
 /* Handler for window re-size event. Called back when the window first appears and
    whenever the window is re-sized with its new width and height */
 void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
-	// Compute aspect ratio of the new window
-	if (height == 0) height = 1;                // To prevent divide by 0
-	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+    // Compute aspect ratio of the new window
+    if (height == 0) height = 1;                // To prevent divide by 0
+    GLfloat aspect = (GLfloat)width / (GLfloat)height;
 
-	// Set the viewport to cover the new window
-	glViewport(0, 0, width, height);
+    // Set the viewport to cover the new window
+    glViewport(0, 0, width, height);
 
-	// Set the aspect ratio of the clipping volume to match the viewport
-	glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-	glLoadIdentity();             // Reset
-	// Enable perspective projection with fovy, aspect, zNear and zFar
-	// gluPerspective(45.0f, aspect, 0.1f, 100.0f);
-	float lengthX = config.lengthX;
-	float lengthY = config.lengthY;
-	float lengthZ = config.lengthZ;
+    // Set the aspect ratio of the clipping volume to match the viewport
+    glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+    glLoadIdentity();             // Reset
+    // Enable perspective projection with fovy, aspect, zNear and zFar
+    // gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+    float lengthX = config.lengthX;
+    float lengthY = config.lengthY;
+    float lengthZ = config.lengthZ;
 
-	auto max_array = { lengthX,lengthY,lengthZ };
-	float max = *std::max_element(max_array.begin(), max_array.end());
+    auto max_array = { lengthX,lengthY,lengthZ };
+    float max = *std::max_element(max_array.begin(), max_array.end());
 
-	std::cout << "max:" << max << std::endl;
+    std::cout << "max:" << max << std::endl;
 
-	config.Set_Ortho_Width_Height(300, 300, 300);
-	//config.Set_Grid_Width_Height();
+    config.Set_Ortho_Width_Height(300, 300, 300);
+    //config.Set_Grid_Width_Height();
 
-	glOrtho(-config.orthoX, config.orthoX, -config.orthoY, config.orthoY, -config.orthoZ, config.orthoZ);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glOrtho(-config.orthoX, config.orthoX, -config.orthoY, config.orthoY, -config.orthoZ, config.orthoZ);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 /* Main function: GLUT runs as a console application starting at main() */
 int main(int argc, char** argv) {
 
-	// auto func = std::bind(&Config::mySpecialKey, &config);
-	glutInit(&argc, argv);            // Initialize GLUT
-	glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-	glutInitWindowSize(800, 800);   // Set the window's initial width & height
-	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-	glutCreateWindow(title);          // Create window with the given title
-	glutDisplayFunc(display);       // Register callback handler for window re-paint event
-	glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-	glutKeyboardFunc(myKeyboard);
-	glutSpecialFunc(mySpecialKey);
-	glutMouseFunc(mouseClicks);
+    // auto func = std::bind(&Config::mySpecialKey, &config);
+    glutInit(&argc, argv);            // Initialize GLUT
+    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
+    glutInitWindowSize(800, 800);   // Set the window's initial width & height
+    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+    glutCreateWindow(title);          // Create window with the given title
+    glutDisplayFunc(display);       // Register callback handler for window re-paint event
+    glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+    glutKeyboardFunc(myKeyboard);
+    glutSpecialFunc(mySpecialKey);
+    glutMouseFunc(mouseClicks);
 
-	config.Set_Grid_Amount(25);
+    config.Set_Grid_Amount(25);
 
-	// 建立右建選單
-	buildPopupMenu();
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-
-
-	initGL();                       // Our own OpenGL initialization
+    // 建立右建選單
+    buildPopupMenu();
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 
-	glutMainLoop();                 // Enter the infinite event-processing loop
+    initGL();                       // Our own OpenGL initialization
 
 
-	return 0;
+    glutMainLoop();                 // Enter the infinite event-processing loop
+
+
+    return 0;
 }
--- Lab1222/Lab1222/Lab08_Rasterization/function.h
+++ Lab1222/Lab1222/Lab08_Rasterization/function.h
@@ -9,108 +9,114 @@
 #include "global.h"
 
 float getSin(float theta) {
-	return (float)sin(theta * M_PI / 180.0);
+    return (float)sin(theta * M_PI / 180.0);
 }
 
 float getCos(float theta) {
-	return (float)cos(theta * M_PI / 180.0);
+    return (float)cos(theta * M_PI / 180.0);
+}
+
+void int_swap(int *a, int*b) {
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
 }
 
 void myKeyboard(unsigned char key, int x, int y) {
-	// std::cout << "key" << std::endl;
-	switch (key) {
-	case 'r':
-		Click = false;
-		config.clickX = 0;
-		config.clickY = 0;
-		config.clickZ = 0;
+    // std::cout << "key" << std::endl;
+    switch (key) {
+    case 'r':
+        Click = false;
+        config.clickX = 0;
+        config.clickY = 0;
+        config.clickZ = 0;
 
-		thetaX = angleX;
-		thetaY = angleY;
-		thetaZ = angleZ;
-		tx = 0;
-		ty = 0;
-		tz = 0;
-		break;
-	case 'a':
-		thetaY -= 3;
-		break;
-	case 'd':
-		thetaY += 3;
-		break;
-	case 'w':
-		thetaX -= 3;
-		break;
-	case 's':
-		thetaX += 3;
-		break;
-	case 'z':
-		thetaZ += 3;
-		break;
-	case 'x':
-		thetaZ -= 3;
-		break;
-	default:
-		break;
-	}
+        thetaX = angleX;
+        thetaY = angleY;
+        thetaZ = angleZ;
+        tx = 0;
+        ty = 0;
+        tz = 0;
+        break;
+    case 'a':
+        thetaY -= 3;
+        break;
+    case 'd':
+        thetaY += 3;
+        break;
+    case 'w':
+        thetaX -= 3;
+        break;
+    case 's':
+        thetaX += 3;
+        break;
+    case 'z':
+        thetaZ += 3;
+        break;
+    case 'x':
+        thetaZ -= 3;
+        break;
+    default:
+        break;
+    }
 
-	// std::cout << "thetaX:" << thetaX << std::endl;
-	glutPostRedisplay();
+    // std::cout << "thetaX:" << thetaX << std::endl;
+    glutPostRedisplay();
 }
 
 void mySpecialKey(int key, int x, int y) {
-	switch (key) {
-	case GLUT_KEY_LEFT:
-		tx -= 2.0f;
-		break;
-	case GLUT_KEY_RIGHT:
-		tx += 2.0f;
-		break;
-	case GLUT_KEY_UP:
-		ty += 2.0f;
-		break;
-	case GLUT_KEY_DOWN:
-		ty -= 2.0f;
-		break;
-	default:
-		break;
-	}
+    switch (key) {
+    case GLUT_KEY_LEFT:
+        tx -= 2.0f;
+        break;
+    case GLUT_KEY_RIGHT:
+        tx += 2.0f;
+        break;
+    case GLUT_KEY_UP:
+        ty += 2.0f;
+        break;
+    case GLUT_KEY_DOWN:
+        ty -= 2.0f;
+        break;
+    default:
+        break;
+    }
 
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
 
 void mouseClicks(int button, int state, int x, int y) {     //當按下滑鼠左鍵時會執行裡面
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		//do something
-		// if (Click == false) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        //do something
+        // if (Click == false) {
 
-		float clickX, clickY;
-		int width = glutGet(GLUT_WINDOW_WIDTH);
-		int height = glutGet(GLUT_WINDOW_HEIGHT);
+        float clickX, clickY;
+        int width = glutGet(GLUT_WINDOW_WIDTH);
+        int height = glutGet(GLUT_WINDOW_HEIGHT);
 
-		if (x > width / 2)
-			clickX = (float)(x - width / 2);
-		else
-			clickX = (float)(-(width / 2 - x));
+        if (x > width / 2)
+            clickX = (float)(x - width / 2);
+        else
+            clickX = (float)(-(width / 2 - x));
 
-		if (y > height / 2)
-			clickY = (float)(-(y - height / 2));
-		else
-			clickY = (float)(height / 2 - y);
+        if (y > height / 2)
+            clickY = (float)(-(y - height / 2));
+        else
+            clickY = (float)(height / 2 - y);
 
-		config.Add_New_Click((int)(clickX * config.orthoX * 2 / width), (int)(clickY * config.orthoY * 2 / height));
+        config.Add_New_Click((int)(clickX * config.orthoX * 2 / width), (int)(clickY * config.orthoY * 2 / height));
 
-		//config.clickX = clickX * config.orthoX * 2 / width;
-		//config.clickY = clickY * config.orthoY * 2 / height;
+        //config.clickX = clickX * config.orthoX * 2 / width;
+        //config.clickY = clickY * config.orthoY * 2 / height;
 
-		//clickX = clickX / width * range;
-		//clickY = clickY / height * range;
+        //clickX = clickX / width * range;
+        //clickY = clickY / height * range;
 
-		std::cout << "Width:" << width << " Height:" << height << " x:" << x << " y:" << y;
-		std::cout << " ClickX:" << config.clickX << " ClickY:" << config.clickY << std::endl;
+        std::cout << "Width:" << width << " Height:" << height << " x:" << x << " y:" << y;
+        std::cout << " ClickX:" << config.clickX << " ClickY:" << config.clickY << std::endl;
 
 
-		//Click = true;
-	//}
-	}
+        //Click = true;
+    //}
+    }
 }
